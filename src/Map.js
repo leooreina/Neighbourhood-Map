654a551ac4f4b229ddf98a18b722dd6ec28f830d
Leonardo Reina <leo.reina01@gmail.com>
2019-07-28 14:10:59 -0300

    Add new funcionalities

--- src/Map.js
+++ src/Map.js
@@ -8,8 +8,8 @@ class Map extends Component {
 
   state = {
     infos: [],
-    search: '',
-    markers: this.infos
+    filteredInfos: [],
+    markers: []
   }
 
   componentDidMount() {
@@ -43,7 +43,8 @@ class Map extends Component {
     axios.get(requestApiUrl + new URLSearchParams(parametersObject))
       .then(response => {
         this.setState({
-          infos: response.data.response.groups[0].items
+          infos: response.data.response.groups[0].items,
+          filteredInfos: response.data.response.groups[0].items
         }, this.loadMap())
       })
       .catch(error => {
@@ -57,42 +58,74 @@ class Map extends Component {
       center: {lat: 43.643819, lng: -79.39779},
       zoom: 11
     })
-    const { search, infos, markers } = this.state
-    const filtered = infos.filter(place => {
+    const { infos } = this.state
+    const filteredInfos = infos.filter(place => {
       this.createDetails(map)
-      return place.venue.name.toLowerCase().indexOf(search.toLowerCase()) !== -1
+      return place.venue.name.toLowerCase().includes(e.target.value.toLowerCase())
     })
-
     this.setState({
-      search: e.target.value,
-      infos: filtered
+      filteredInfos
+    }, () => {
+      this.createDetails(map)
     })
   }
 
   createDetails = (map) => {
-    const { infos } = this.state
-    infos.map(info => {
+    const { filteredInfos, markers } = this.state
+    filteredInfos.map(info => {
       let infoWindow = new window.google.maps.InfoWindow()
       let popupMessage =`<div><h3>${info.venue.name}</h3><p>${info.venue.location.address}</p></div>`
       let marker = new window.google.maps.Marker({
         position: {lat: info.venue.location.lat, lng: info.venue.location.lng},
         map: map,
+        id: info.venue.id,
         title: info.venue.name,
         animation: window.google.maps.Animation.DROP
       })
       marker.addListener('click', function() {
+        if (marker.getAnimation() !== null) {
+          marker.setAnimation(null)
+        } else {
+          marker.setAnimation(window.google.maps.Animation.BOUNCE)
+        }
+        setTimeout(() => { marker.setAnimation(null)}, 1500)
         infoWindow.setContent(popupMessage)
         infoWindow.open(map, marker)
       })
+      markers.push(marker)
     })
   }
 
-  render() {
-    const { search, infos } = this.state
-    const filteredPlaces = infos.filter(place => {
-      return place.venue.name.toLowerCase().indexOf( search.toLowerCase() ) !== -1
-    })
+  /* A method that opens the infowindow in map when a element is clicked on list */
+  listItemCheck = (place) => {
+    // Check if the marker id match the place id
+    let marker = this.state.markers.filter(m => m.id === place.id)[0]
 
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: place.location.lat, lng: place.location.lng},
+      zoom: 11
+    })
+    let newMarker = new window.google.maps.Marker({
+      position: {lat: place.location.lat, lng: place.location.lng},
+      map: map,
+      id: place.id,
+      title: place.name,
+      animation: window.google.maps.Animation.DROP
+    })
+    let infoWindow = new window.google.maps.InfoWindow()
+    let popupMessage =`<div><h3>${place.name}</h3><p>${place.location.address}</p></div>`
+    if (newMarker.getAnimation() !== null) {
+      newMarker.setAnimation(null)
+    } else {
+      newMarker.setAnimation(window.google.maps.Animation.BOUNCE)
+    }
+    setTimeout(() => { newMarker.setAnimation(null)}, 1500)
+    infoWindow.setContent(popupMessage)
+    infoWindow.open(map, newMarker)
+  }
+
+  render() {
+    const { filteredInfos } = this.state
     return (
       <div id="app">
         <nav id="header-search" aria-label="Search in Map" role="search">
@@ -105,8 +138,8 @@ class Map extends Component {
           <div id="list" aria-label="Places List" role="navigation">
             <h3 className="subtitle-map">Places List</h3>
             <ul className="places-list">
-              {filteredPlaces.map(place => (
-                <li className="list-item" key={place.venue.id}>
+              {filteredInfos.map(place => (
+                <li className="list-item" key={place.venue.id} tabIndex="0" onClick={() => { this.listItemCheck(place.venue) } }>
                   <h4 className="place-name">{place.venue.name}</h4>
                 </li>
               ))}
